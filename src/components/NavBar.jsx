37fa4ba1c418ce6cdb77129e16952f73690918a3
Brightky <brightkyefoo@gmail.com>
2022-09-10 21:22:28 +0100

    yoooo

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Menu} from '@mui/icons-material/';
+
+const NavBar = () => {
+  return (
+    <nav>
+        <Menu  />
+    </nav>
+  )
+}
+
+export default NavBar
